c14a2e03032932222ef7080ebcd209ac523ac5b8
zbyszeg <zbyszek.gora@gmail.com>
2016-08-24 19:45:03 +0200

    wyszukiwanie

---
+++ bib/katalog_bib.php
@@ -0,0 +1,15 @@
+<div id="szukaj">
+	Wyszukaj pozycję wg:<br />
+	<form name="formularz_szukania" method="post" action="Pozycje"> 
+	<select name="kryterium">
+	  <option value="autor">autora</option>
+	  <option value="tytul">tytułu</option>
+	  <option value="wydaw">wydawnictwa</option>
+	  <option value="rok_wyd">roku wydania</option>
+	  <option value="nr_ewid">numeru ewidencyjnego</option>
+	  <option value="czytelnik">czytelnika</option>
+	</select>
+
+	<input name="wartosc" type="text">
+	<input type="submit" name="Submit" value="Szukaj" style="width: 100px;">
+</div>
