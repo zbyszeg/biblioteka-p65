3cb2dcfbf338b4eb5bad253987c35341aed9b2e2
zbyszeg <zbyszek.gora@gmail.com>
2016-08-24 11:06:43 +0200

    powrót

--- main/main.php
+++ main/main.php
@@ -1,32 +1,14 @@
-<?php
-	session_start();
-	
-	if ((isset($_SESSION['zalogowany'])) && ($_SESSION['zalogowany']==true))
-	{
-		header('Location: Bibliotekarz');
-		exit();
-	}
-?>
-
 <div id="mainp">
 	<div id="forma">
-		<form action="Logowanie" method="post">
+		<form>
 				
-			<input type="text" name="login" placeholder="login" onfocus="this.placeholder=''" onblur="this.placeholder='login'" >
+			<input type="text" placeholder="login" onfocus="this.placeholder=''" onblur="this.placeholder='login'" >
 				
-			<input type="password" name="haslo" placeholder="hasło" onfocus="this.placeholder=''" onblur="this.placeholder='hasło'" >
+			<input type="password" placeholder="hasło" onfocus="this.placeholder=''" onblur="this.placeholder='hasło'" >
 				
 			<input type="submit" value="Zaloguj się">
 				
-		</form>
-		<?php
-			if(isset($_SESSION['blad']))
-			{
-				echo $_SESSION['blad'];
-				unset($_SESSION['blad']);
-			}
-		?>
-		<br/>
+		</form><br/>
 		<img src="img/logoP.gif" style="width: 150px"/>
 	</div>
 	<br /><br />
