8ce4e8ccb932c072007864c6055d75c2067eee27
zbyszeg <zbyszek.gora@gmail.com>
2016-08-24 19:17:17 +0200

    menu

--- main/operator.php
+++ main/operator.php
@@ -9,18 +9,33 @@
 	}
 	
 ?>
+<div id="kontener">
+		<nav id="menubar">
+			
+			<?php echo '<a href="Wyloguj">Wyloguj się!</a>'; ?><br />
+			<hr />
+			<a href="#">Wyszukaj pozycje</a><br />
+			<a href="#">Dodaj pozycję</a><br />
+			<a href="#">Edytuj pozycję</a><br />
+			<a href="#">Usuń pozycję</a><br />
+			<hr />
+			<a href="#">Wypożycz książkę</a><br />
+			<a href="#">Oddaj książkę</a><br />
+		</nav>
+		<section id="mainbib">
+			<?php
+				$page = isset($_GET['b']) ? basename($_GET['b']) : 'katalog_bib';
+ 				$file = 'main/' . $page . '.php';
+ 				include $file;
+			?>
+		</section>
+		<div style="clear: both;"></div>
+	</div>
+
+
+
+
 
 <div id="menubar">
-<?php
-	echo '<a href="Wyloguj">Wyloguj się!</a>';
-?>
-</div>
-<div id="mainbib">
-	<?php
-		$page = isset($_GET['b']) ? basename($_GET['b']) : 'katalog_bib';
- 
-		$file = 'main/' . $page . '.php';
- 
-		include $file;
-	?>
+
 </div>
