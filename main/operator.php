4bdb3d4a5ce07e6e18b1148ba011a53f9b6f28c1
zbyszeg <zbyszek.gora@gmail.com>
2016-08-24 15:04:30 +0200

    strona operatora

--- main/operator.php
+++ main/operator.php
@@ -13,7 +13,16 @@
 <div id="menubar">
 <?php
 
-	echo '<p>[ <a href="Wyloguj">Wyloguj się!</a> ]</p>';
+	echo '<a href="Wyloguj">Wyloguj się!</a>';
 	
 ?>
+</div>
+<div id="mainbib">
+	<?php
+		$page = isset($_GET['b']) ? basename($_GET['b']) : 'szukaj_bib';
+ 
+		$file = 'main/' . $page . '.php';
+ 
+		include $file;
+	?>
 </div>
---
+++ main/szukaj_bib.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+</head>
+<body>
+	<div id="wyniki">
+		<a href="Przeglądaj">Powrót do wyszukiwarki</a><br /><br />
+		<hr />
+		<?php
+
+		//Nawiazujemy polaczenie z baza, podajac adres hosta, login oraz haslo.
+		$conn=mysqli_connect("mysql.hostinger.pl", "u432478404_bibli", "65Perełka");
+
+		//Wybieramy interesujaca nas baze.
+		mysqli_select_db($conn,"u432478404_bibli");
+		
+		//ustawiamy poprawne wyświetlanie
+		mysqli_query($conn,"SET NAMES 'utf8' COLLATE 'utf8_polish_ci'");
+
+		$kryterium = $_POST['kryterium'];
+		$wartosc = $_POST['wartosc'];
+
+		switch ($kryterium)
+		{
+		  case "autor":  
+		  $result=mysqli_query($conn,"select * from ksiazki where autor like '%$wartosc%'");
+		  break;
+		 
+		  case "tytul":
+		  $result=mysqli_query($conn,"select * from ksiazki where tytul like '%$wartosc%'");
+		  break;
+		}
+
+		/* 
+		Przy uzyciu petli 'while', kolejne odpowiadajace kryterium wyszukiwania 
+		rekordy sa pobierane z bazy i zapisywane jako pola tablicy asocjacyjnej $myrow.
+		Jej klucze odpowiadaja nazwom kolumn tabeli w bazie. 
+		*/
+
+		while ($myrow = mysqli_fetch_assoc($result))
+			{
+
+		//Wyswietlamy kolejne elementy tablicy.
+			echo "<p>";
+			echo "Nr ewidencyjny: ".$myrow["nr_ewidencyjny"];
+			echo "<br>Autor: ".$myrow["autor"];
+			echo "<br>Tytuł: ".$myrow["tytul"];
+			
+			switch ($myrow["id_statusu"])
+			{
+				case 1:
+					echo '<br>Status: <span style="color: green">Dostępna</span>';
+				break;
+				
+				case 2:
+					echo '<br>Status: <span style="color: red">Wypożyczona</span>';
+				break;
+			}	
+			echo "</p>";
+			echo "<hr />";
+			}
+		?>
+	</div>	
+</body>
+</html>
