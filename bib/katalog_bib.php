1675c2be5757a732493c6bdd6e59da3ef5c59e33
zbyszeg <zbyszek.gora@gmail.com>
2016-08-25 11:03:35 +0200

    bio

--- bib/katalog_bib.php
+++ bib/katalog_bib.php
@@ -1,6 +1,6 @@
 <div id="szukaj">
 	Wyszukaj pozycję wg:<br />
-	<form name="formularz_szukania" method="post" action="?b=szukaj_bib"> 
+	<form name="formularz_szukania" method="post" action="Wyniki"> 
 	<select name="kryterium">
 	  <option value="autor">autora</option>
 	  <option value="tytul">tytułu</option>
