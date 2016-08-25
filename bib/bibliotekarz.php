da896a7bafabc9fb466e68da2ebb2cb865d2f5be
zbyszeg <zbyszek.gora@gmail.com>
2016-08-25 10:46:08 +0200

    bio

--- bib/bibliotekarz.php
+++ bib/bibliotekarz.php
@@ -41,7 +41,7 @@
 			
 			<?php echo '<a href="../logout.php">Wyloguj się!</a>'; ?><br />
 			<hr />
-			<a href="Wyszukaj">Wyszukaj pozycje</a><br />
+			<a href="?b=katalog_bib">Wyszukaj pozycje</a><br />
 			<a href="#">Dodaj pozycję</a><br />
 			<a href="#">Edytuj pozycję</a><br />
 			<a href="#">Usuń pozycję</a><br />
