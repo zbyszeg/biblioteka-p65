5abfeb820a9c8639fc8ae7eeb1be40ccbafbaec0
zbyszeg <zbyszek.gora@gmail.com>
2016-08-25 10:41:14 +0200

    bio

--- bib/bibliotekarz.php
+++ bib/bibliotekarz.php
@@ -20,17 +20,17 @@
 	<meta name="description" content="Katalog książek biblioteki Przedszkola nr 65 Perełka w Krakowie">
 	<meta name="author" content="Zbyszek Góra">
 	
-	<link rel="stylesheet" href="css/style.css">
-	<link rel="stylesheet" href="css/fontello.css" type="text/css">
+	<link rel="stylesheet" href="../css/style.css">
+	<link rel="stylesheet" href="../css/fontello.css" type="text/css">
 	<link href="https://fonts.googleapis.com/css?family=Archivo+Narrow:400,700|Dekko|Oxygen:400,700|Rubik:400,700,900&subset=latin-ext" rel="stylesheet">
-	<link rel="icon" href="fav/favicon.ico" type="image/x-icon">
+	<link rel="icon" href="../fav/favicon.ico" type="image/x-icon">
 	
-	<script src="js/jquery-3.1.0.js"></script>
-	<script src="js/sticky.js"></script>
+	<script src="../js/jquery-3.1.0.js"></script>
+	<script src="../js/sticky.js"></script>
 </head>
 <body>
 	<div id="menu">
-			<div id="dom"><a href="StronaGłówna"><i class="icon-home"></i></a></div>
+			<div id="dom"><a href="../index.php"><i class="icon-home"></i></a></div>
 			<a href="http://perelkakrakow.pl/" target="=blank"><div id="logo"><i class="icon-home-1"></i></div></a>
 			<div id="mail"><a href="mailto:przedszkole-65@wp.pl"><i class="icon-mail"></i></a></div>
 			<a href="https://www.facebook.com/perelkakrakow" target="=blank"><div id="fb"><i class="icon-facebook-1"></i></div></a>
